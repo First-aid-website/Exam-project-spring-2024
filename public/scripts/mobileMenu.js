d01d2dcb7b60c9d0a6f32ab1814f647aded66dcd
Walocial <walocial@hotmail.com>
2024-04-23 12:10:31 +0200

    Styling of index page
    Changed a lot of minor details and corrected some minor issues
    
    - Further styled the index page

--- public/scripts/mobileMenu.js
+++ public/scripts/mobileMenu.js
@@ -1,11 +1,17 @@
 const mobileMenuBtn = document.getElementById('mobileMenuBtn');
 const mobileMenu = document.getElementById('mainMenu');
+let burgerMenu = 'fa-bars';
+let closeMenu = 'fa-x';
 
 function hideMobileMenu() {
     mobileMenu.classList.add('hidden');
+    mobileMenuBtn.classList.remove(closeMenu);
+    mobileMenuBtn.classList.add(burgerMenu);
 }
 function showMobileMenu() {
     mobileMenu.classList.remove('hidden')
+    mobileMenuBtn.classList.remove(burgerMenu);
+    mobileMenuBtn.classList.add(closeMenu);
 }
 function toggleMobileMenu() {
     if(mobileMenu.classList.contains('hidden')){
