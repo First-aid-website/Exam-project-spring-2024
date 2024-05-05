551426ddcf4ea41c6e7eb9ee006f5402449327b9
Walocial <Walocial@hotmail.com>
2024-05-06 01:20:45 +0200

    More client side input sanitization
    - Added sanitization for number inputs and titles
    - Will optimize the code in a coming commit

--- public/scripts/inputSanitization.js
+++ public/scripts/inputSanitization.js
@@ -1,9 +1,14 @@
-const USERNAME_LOGIN = document.getElementById('username');
-const NAME_FORM = document.getElementById('name');
+const USERNAME_INPUT = document.getElementById('username');
+const NAME_INPUT = document.getElementById('name');
+const TITLE_INPUT = document.getElementById('title');
+const NUMBER_INPUT = document.querySelectorAll('.numberInput');
 const USERNAME_REGEX = /[^a-zæøåA-ZÆØÅ0-9]/g;
-const NAME_REGEX = /[^a-zæøåA-ZÆØÅ\s]/g;
+const LETTERS_ONLY_REGEX = /[^a-zæøåA-ZÆØÅ\s]/g;
+const NUMBERS_ONLY_REGEX = /[^\d]/g;
 const MAX_USERNAME_LENGTH = 20;
 const MAX_NAME_LENGTH = 50;
+const MAX_TITLE_LENGTH = 150;
+const MAX_NUMBER_LENGTH = 5;
 
 function restrictInput(input, regex, maxLength) {
     let value = input.value;
@@ -17,14 +22,28 @@ function restrictInput(input, regex, maxLength) {
     input.value = value;
 }
 
-if (USERNAME_LOGIN) {
-    USERNAME_LOGIN.addEventListener("input", function() {
-        restrictInput(USERNAME_LOGIN, USERNAME_REGEX, MAX_USERNAME_LENGTH);
+if (USERNAME_INPUT) {
+    USERNAME_INPUT.addEventListener("input", function() {
+        restrictInput(USERNAME_INPUT, USERNAME_REGEX, MAX_USERNAME_LENGTH);
     });
 }
 
-if (NAME_FORM) {
-    NAME_FORM.addEventListener("input", function() {
-        restrictInput(NAME_FORM, NAME_REGEX, MAX_NAME_LENGTH);
+if (NAME_INPUT) {
+    NAME_INPUT.addEventListener("input", function() {
+        restrictInput(NAME_INPUT, LETTERS_ONLY_REGEX, MAX_NAME_LENGTH);
+    });
+}
+
+if (TITLE_INPUT) {
+    TITLE_INPUT.addEventListener("input", function() {
+        restrictInput(TITLE_INPUT, LETTERS_ONLY_REGEX, MAX_TITLE_LENGTH);
+    });
+}
+
+if (NUMBER_INPUT) {
+    NUMBER_INPUT.forEach(function(input) {
+        input.addEventListener("input", function() {
+            restrictInput(input, NUMBERS_ONLY_REGEX, MAX_NUMBER_LENGTH);
+        });
     });
 }
